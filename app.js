176ebb95cb83f372898c9906e0af7c283afad2a2
Christine Bonk <christinebonk@gmail.com>
2018-10-29 21:10:51 -0400

    get accounts and balances

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ var keys = require("./keys.js");
 
 var key = keys.questrade.consumer_key;
 
+function getAccounts() {
+	axios.get("https://api02.iq.questrade.com/v1/accounts", {
+		headers: {Authorization: "Bearer " + key}
+	}).then(function(res) {
+		var margin = res.data.accounts[0].number;
+		var rrsp = res.data.accounts[1].number;
+		console.log(margin);
+		console.log(rrsp);
+		getBalance(margin);
+		getBalance(rrsp);
+	}).catch(function(error) {
+		console.log(error);
+	})
+}
+
+getAccounts();
 
 
-axios.get("https://api02.iq.questrade.com/v1/accounts", {
-	headers: {Authorization: "Bearer " + key}
-}).then(function(res) {
-	console.log(res.data)
-}).catch(function(error) {
-	console.log(error);
-})
 
+function getBalance(account) {
+	axios.get(`https://api02.iq.questrade.com/v1/accounts/${account}/balances`, {
+		headers: {Authorization: "Bearer " + key}
+	}).then(function(res){
+		console.log(res.data.perCurrencyBalances[0].totalEquity);
+	}).catch(function(error) {
+		console.log(error);
+	}) 
+}
 
