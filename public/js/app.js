0166c5bf2ca5e1e803632a2b8aaf27766c05886d
Christine Bonk <christinebonk@gmail.com>
2018-11-04 20:42:31 -0500

    set-up html for amount input

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,16 @@
+//display accounts on load
 $.ajax("/api/accounts", {
 	type: "GET"
 }).then(function(res) {
 	var accounts = res;
-	console.log(accounts)
 	for (var key in accounts) {
-		console.log(key);
-		console.log(accounts[key])
+		//todo: loading screen
 		var selectionDiv = $(`<div class="selection">`);
 		var selectionInput = $(`<input id=${key} value=${accounts[key]} name='accounts' type='radio' defaultChecked>`);
 		var selectionLabel = $(`<label for=${key}>${key}</label>`);
 		selectionDiv.append(selectionInput, selectionLabel);
 		$("#single-select").append(selectionDiv);
 	}
-})
+});
+
+
