4c98add7edefe1be5bdd29a72fa3c35b64447609
Christine Bonk <christinebonk@gmail.com>
2018-11-04 20:32:57 -0500

    return account information to browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
-import { getAccounts } from './test.js';
-
-getAccounts();
+$.ajax("/api/accounts", {
+	type: "GET"
+}).then(function(res) {
+	var accounts = res;
+	console.log(accounts)
+	for (var key in accounts) {
+		console.log(key);
+		var selectionDiv = $(`<div class="selection">`);
+		var selectionInput = $(`<input id=${key} value=${res[key]} name='accounts' type='radio' defaultChecked`);
+		var selectionLabel = $(`<label for=${key}>${key}</label>`);
+		selectionDiv.append(selectionInput, selectionLabel);
+		$("#accounts").append("hello");
+	}
+})
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,76 @@
 var bodyparser = require("body-parser");
 const path = require("path");
+require("dotenv").config();
+var axios = require("axios");
+var keys = require("../keys.js");
+var refresh = keys.questrade.refresh_token;
+var access = keys.questrade.access_token;
+var server = keys.questrade.server;
+var fs = require("fs");
+var portfolio;
+var positions;
 
+//routes
 function routes(app) {
 	app.get("/", function(req,res) {
 		res.sendFile(path.join(__dirname, "../index.html"));
 	});
+
+	app.get("/api/accounts", function(req,res) {
+		axios.get(`${server}v1/accounts`, {
+			headers: {Authorization: "Bearer " + access}
+		}).then(function(results) {
+			var margin = results.data.accounts[0].number;
+			var rrsp = results.data.accounts[1].number;
+			res.json({Margin:margin, RRSP:rrsp});
+		}).catch(function(error) {
+			if (error.response.data.code == "1017") {
+				refreshToken();
+			} else {
+				console.log(error);
+			};
+		});
+	});
+
+	app.get("/api/balance", function(req,res) {
+		axios.get(`${server}v1/accounts/${account}/balances`, {
+			headers: {Authorization: "Bearer " + access}
+		}).then(function(res){
+			equity = res.data.perCurrencyBalances[0].totalEquity;
+			// getPositions(account, equity);
+		}).catch(function(error) {
+			console.log(error);
+		}); 
+	});
+
 }
 
 module.exports = routes;
+
+//refresh token
+function refreshToken() {
+	axios.get(`https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=${refresh}`)
+	.then(function(res) {
+		console.log(res);
+		fs.writeFile(".env", 
+			`REFRESH_TOKEN=${res.data.refresh_token}\nACCESS_TOKEN=${res.data.access_token}\nSERVER=${res.data.api_server}`
+			, function(error,data) {
+			if(error) {
+				return console.log(error);
+			};
+			access = res.access_token;
+		});
+	}).catch(function(error) {
+		console.log(error);
+	});
+};
+
+//pull portfolio
+function getPortfolio() {
+	fs.readFile("portfolio.txt", "utf8", function(error,data) {
+		if(error) {
+			return console.log(error);
+		};
+		portfolio = JSON.parse(data);	
+	});
+}
