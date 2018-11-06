5035a02356935655cf6a3da6deae10acc20af598
Christine Bonk <christinebonk@gmail.com>
2018-11-05 21:32:08 -0500

    capture user input

--- public/js/app.js
+++ public/js/app.js
@@ -6,11 +6,17 @@ $.ajax("/api/accounts", {
 	for (var key in accounts) {
 		//todo: loading screen
 		var selectionDiv = $(`<div class="selection">`);
-		var selectionInput = $(`<input id=${key} value=${accounts[key]} name='accounts' type='radio' defaultChecked>`);
+		var selectionInput = $(`<input id=${key} value=${accounts[key]} name='accounts' type='radio'>`);
 		var selectionLabel = $(`<label for=${key}>${key}</label>`);
 		selectionDiv.append(selectionInput, selectionLabel);
 		$("#single-select").append(selectionDiv);
 	}
 });
 
-
+//submit button functionality
+$("#submit").on("click", function(event) {
+	event.preventDefault();
+	var amount = $("#enter-amount").val().trim();
+	var account = $("input[name='accounts']:checked").val();
+	console.log(account)
+})
